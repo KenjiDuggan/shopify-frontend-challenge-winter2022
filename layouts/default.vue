b825c26659bcab1156b0886c688d631f647357bc
KenjiDuggan <k_anci@live.concordia.ca>
2021-09-14 18:14:43 -0400

    saving state and moving to desktop env got my modem finally

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,61 +1,22 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app id="appBg">
     <v-app-bar
+      id="navBg"
       :clipped-left="clipped"
       fixed
-      app
+      app 
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+      <v-toolbar-title class="yellowCl" v-text="title" />
+      <v-toolbar-title v-text="subtitle" />  
+        <v-text-field
+          id="searchbar"
+          v-model="search"
+          label="Solo"
+          prepend-icon="mdi-magnify"
+          placeholder="Placeholder"
+          solo
+        ></v-text-field>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -110,8 +71,35 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'SPACE',
+      subtitle: 'STAGRAM'
     }
   }
 }
 </script>
+
+<style>
+#appBg {
+  background-color: #121620;
+}
+
+#navBg {
+    background-color: #121620;
+}
+
+.yellowCl {
+  color: #fec001;
+}
+
+.yellowBg {
+  background-color: #fec001;
+}
+
+.mdi-magnify {
+  fill: grey;
+}
+
+#searchbar {
+  padding-top: 20px;
+}
+</style>
