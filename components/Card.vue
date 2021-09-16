c35ecd05c23caac4f61797f76acde09d1bcad1c0
KenjiDuggan <kenjiduggan@gmail.com>
2021-09-15 23:55:55 -0400

    main card styling done and missing shopping cart for export of liked images, say

---
+++ components/Card.vue
@@ -0,0 +1,200 @@
+<template>
+    <div class="card">
+        <img :src="imgSrc" alt="" class="card__image">
+        <div class="card__text-wrapper">
+        <h2 class="card__title">{{ cameraName }}</h2>
+        <div class="card__post-date">Earth Date of Capture: {{ earthDate }}</div>
+            <div :key="isLiked" class="card__details-wrapper">
+                <div class="card__post-date">Landing: {{ landingDate }}</div>
+                <div class="card__post-date">Launch: {{ launchDate }}</div>
+                <p class="card__excerpt">Full Name: {{ fullName }}</p>
+                <button v-if="!isLiked" class="card__read-more" @click.native="toggleLike()">
+                    <img :src="likeIcon" class="likeIcon" />
+                </button>
+                <button v-else class="card__read-more" @click.native="toggleLike()">
+                    <img :src="dislikeIcon" class="likeIcon" />
+                </button>
+            </div>  
+        </div>
+    </div>
+</template>
+
+<script>
+import likeIcon from '../static/images/thumb_up_black_24dp.svg'
+import dislikeIcon from '../static/images/thumb_down_black_24dp.svg'
+ 
+export default {
+    name: 'Card',
+    props: {
+        imgSrc: {
+            type: String,
+            default: () => ''
+        },
+        cameraName: {
+            type: String,
+            default: () => ''
+        },
+        earthDate: {
+            type: String,
+            default: () => ''
+        },
+        landingDate: {
+            type: String,
+            default: () => ''
+        },
+        launchDate: {
+            type: String,
+            default: () => ''
+        },
+        fullName: {
+            type: String,
+            default: () => ''
+        }
+    },
+    data() {
+        return {
+            likeIcon,
+            dislikeIcon,
+            isLiked: false
+        }
+    },
+    methods: {
+        toggleLike() {
+            console.log("I have been toggled....")
+            const currState = this.isLiked
+            this.isLiked = !currState
+        },
+    }
+}
+</script>
+<style>
+.card {
+  border: 0px solid aqua;
+  margin: 0.5rem;
+  position: relative;
+  height: 20rem;
+  overflow: hidden;
+  border-radius: 0.5rem;
+  flex: 1;
+  min-width: 290px;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
+  -webkit-backface-visibility: hidden;
+  -moz-backface-visibility: hidden;
+  -webkit-transform: translate3d(0, 0, 0);
+  -moz-transform: translate3d(0, 0, 0);
+}
+ 
+.card::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0) linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.7) 80%);;
+  z-index: 0;
+}
+
+.card__card-link {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+
+.card__image {
+  width: 100%;
+  height: 100%;
+  display: block;
+  object-fit: cover;
+  transition: transform 3s ease;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  position: relative;
+  z-index: -1;
+}
+
+.card__text-wrapper {
+  position: absolute;
+  bottom: 0rem;
+  padding: 1rem;
+  color: white;
+  width: 100%;
+  transition: background-color 1.5s ease;
+}
+
+.card__title {
+  transition: color 1s ease;
+  margin-bottom: .5rem;
+}
+
+.card__post-date {
+  font-size: .7rem;
+  margin-bottom: .5rem;
+  color: #CCC;
+}
+
+.card__details-wrapper {
+  max-height: 0;
+  opacity: 0;
+  transition: max-height 1.5s ease, opacity 1s ease;
+}
+
+.card__excerpt {
+  font-weight: 300;
+}
+
+.card__read-more {
+  background: black;
+  color: #bbb;
+  display: block;
+  padding: 0.4rem 0.6rem;
+  border-radius: 0.3rem;
+  margin-top: 1rem;
+  border: 1px solid #444;
+  font-size: 0.8rem;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  text-decoration: none;
+  width: 7rem;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  z-index: 5;
+}
+
+.card__read-more i {
+  position: relative;
+  left: 0.2rem;
+  color: #888;
+  transition: left 0.5s ease, color 0.6s ease;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.card__read-more:hover i {
+  left: 0.5rem;
+  color: yellow;
+}
+
+.card:hover .card__details-wrapper {
+  max-height: 20rem;
+  opacity: 1;
+}
+
+.card:hover .card__text-wrapper {
+  background-color: rgba(0, 0, 0, 0.6);
+}
+
+.card:hover .card____title {
+  color: yellow;
+}
+
+.card:hover .card__image {
+      transform: scale(1.2);
+      z-index: -1;
+}
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -25,9 +25,8 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
+  plugins: [{ src: '~/plugins/persistedState.client.js' }],
+  
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
@@ -37,6 +36,8 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    // https://www.npmjs.com/package/@nuxtjs/dotenv
+    '@nuxtjs/dotenv'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
